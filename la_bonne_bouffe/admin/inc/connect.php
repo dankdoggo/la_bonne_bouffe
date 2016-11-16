fa7f82089428b751c5b294512cecb28d868420a2
dankdoggo <duhfanofdoge@gmail.com>
2016-11-16 16:44:56 +0100

    fichiers

--- la_bonne_bouffe/admin/inc/connect.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$bdd = new PDO('mysql:host=localhost;dbname=la_bonne_bouffe;charset=utf8', 'root', '');
--- la_bonne_bouffe/inc/connect.php
+++ la_bonne_bouffe/inc/connect.php
@@ -1,2 +1,3 @@
+<?php
 
 $bdd = new PDO('mysql:host=localhost;dbname=la_bonne_bouffe;charset=utf8', 'root', '');
