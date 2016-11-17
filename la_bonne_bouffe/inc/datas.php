f8e4db6aebcdb5399ae7e6913e8408d69ffee8f4
melanie32 <dupuymelanie87@yahoo.fr>
2016-11-17 15:13:55 +0100

    a

--- la_bonne_bouffe/inc/datas.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$mimeTypeAllow = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']; // tableau qui stocke les extensions autorisées à l'upload pour les fichiers du slider recette of index.php
