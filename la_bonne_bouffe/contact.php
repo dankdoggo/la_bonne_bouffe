894bd014499356ec49294829d2c319e7485c60bb
Carinette33 <gtcarine@yahoo.fr>
2016-11-17 10:11:44 +0100

    verif PHP + requete SQL
    sur la page contact.php (formulaire du front)

---
+++ la_bonne_bouffe/contact.php
@@ -0,0 +1,199 @@
+<?php
+
+
+require_once 'inc/connect.php';
+require_once 'inc/functions.php'; 
+
+
+$errors = [];
+$post = [];
+$formValid = false;
+$hasError = false;
+
+
+if (!empty($_POST)) {
+
+	foreach ($_POST as $key => $value) {
+		$post[$key] = trim(strip_tags($value));
+	} // fermeture de mon nettoyage de $POST
+
+	if (empty($post['FirstName-take']) || !minAndMaxLenght($post['FirstName-take'], 3, 30 )){
+		$errors [] = 'Merci d\'indiquer un prénom entre  trois  et trente caractères';
+	}
+
+	if (empty($post['LastName-take']) || !minAndMaxLenght($post['LastName-take'], 3, 30 )){
+		$errors [] = 'Merci d\'indiquer un prénom entre trois et trente caractères';
+	}
+
+	if(!filter_var(($post['Email-take']),FILTER_VALIDATE_EMAIL)){
+		$errors [] = 'Votre adresse email n\'est pas correcte';
+	}
+
+	if (empty($post['Message-take']) || !minAndMaxLenght($post['Message-take'], 3, 100 )){
+		$errors [] = 'Merci d\'indiquer un message entre trois et cent caractères';
+	}
+
+	if(count($errors) === 0){
+		$add=$bdd->prepare ('INSERT INTO lbb_contact (firstname, lastname, email, message) VALUES (:firstname, :lastname, : email, :message)');
+
+		$add->bindValue(':firstname',$post['FirstName-take']);
+		$add->bindValue(':lastname',$post['LastName-take']);
+		$add->bindValue(':email',$post['Email-take']);
+		$add->bindValue(':message',$post['Message-take']);
+
+		if($add->execute()){
+			$formValid = true;
+		}
+
+	} /*fermeture count error ==O*/
+
+	else {
+		$hasError = true;
+	}
+
+
+} /*fermeture première condition if!empty*/
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+	<head>
+
+		<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="TEXTE">
+        <meta name="author" content="TEXTE">
+        <meta name="robots" content="index, follow, archive">
+
+        <!--META FB-->
+        <meta property="og:title" content="TEXTE">
+        <meta property="og:description" content="TEXTE">
+        <meta property="og:local" content="fr-FR">
+        <meta property="og:site_name" content="TEXTE">
+        <meta property="og:image" content="chemin/acces/url.jpg">
+        <meta property="og:type" content="website-article">
+
+
+
+		<title> Contacter la bonne bouffe </title>
+
+	 	<!-- Police Google Font -->
+	    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet"> 
+	    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet"> 
+	            
+	     <!--Icone FontAwesome CDN Bootstrape-->
+	    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous"> 
+	         
+	     <!--Feuille de style Bootstrape-->
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+		 <!-- My CSS -->
+        <link href="css/style.css" rel="stylesheet">
+	</head>
+
+	<body>
+		<div class="wrapper"> <!-- Wrapper comprenant header + main + footer -->
+	       
+	        <header id="header">
+	        	<div class="bloc-contact">
+	        		<div class="titre-contact"><strong>La bonne bouffe</strong></div>
+	        		<div class="info-contact"> 1 rue de l'avenue - 33000 Bordeaux </div>
+	        		<div class="info-contact"> 01.23.45.67.89</div>
+	        	</div>
+
+	      	 	<nav></nav>
+
+	      	 	<div class="nous-contacter">Nous contacter</div>
+	        	
+	        </header>
+
+
+			<main>
+
+				<div class="wrapper-form">
+
+					<h1 class="h1-contact text-center"> Envoyez-nous votre message</h1>
+
+					<p class="text-contact text-center">
+	        		Vous souhaitez réserver le restaurant pour une soirée privée ? <br>
+	        		Prendre un cours de cuisine avec notre Chef ? <br>
+	        		<br>
+	        		Nous traiterons avec grand plaisir votre demande.
+	        		<br> Envoyez-nous votre message directement avec ce formulaire !
+	        		</p>
+
+					<form method="POST" class="contact">
+
+					<!-- Prénom -->
+						<label for="FirstName" class="label-contact">Prénom</label>
+						<input type="text" id="FirstName" name="FirstName-take" placeholder="Ex: Axel" class="input-contact">
+
+					<!-- Nom -->
+						<label for="LastName" class="label-contact">Nom</label>
+						<input type="text" id="LastName" name="LastName-take" placeholder="Ex: Wargnier" class="input-contact">
+					
+
+					<!-- Mail -->
+						<label for="Email" class="label-contact">Mail</label>
+						<input type="email" id="Email" name="Email-take" placeholder="Ex: AxelWargnier@yahoo.fr" class="input-contact">
+						
+
+					<!-- Message -->
+						<label for="Message" class="label-contact">Message</label>
+						<textarea id="Message" name="Message-take" placeholder="Bonjour, je me permets de vous contacter afin d'obetnir un devis pour une soirée privée" class="input-contact textarea-contact"></textarea>
+								
+					<!-- Bouton Submit-->
+						<div class="center-block">
+							<button type="sumbmit" class="boutton-contact"> Envoyer</button>
+						</div>
+
+					</form>
+
+					<h1 class="h1-contact text-center"> Vous aussi pouvez nous contacter directement</h1>
+
+					<p class="text-contact text-center">
+	        		La bonne bouffe
+	        		<br>
+	        		Rue de la gastronomie - Quartier Saint Michel -33000 Bordeaux
+	        		<br>
+	        		01.23.45.67.89
+	        		</p>
+
+
+				</div><!--  fermeture du wrapper-form -->
+
+
+			</main>
+
+
+
+			<footer id="footer">
+
+				<div>Mentions Légales</div>
+				<div>Made with &hearts; by Group 3</div>
+				<div>PICTO FB</div>
+
+			</footer>
+
+
+
+		</div>
+
+
+
+
+
+
+	     </div> <!-- fermeture wrapper comprenant header + main + footer -->  
+
+
+	</body>
+
+
+</html>
