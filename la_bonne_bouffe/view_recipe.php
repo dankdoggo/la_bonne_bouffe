b691b583df796d51c09dbbd551772ca85609d8b2
dankdoggo <duhfanofdoge@gmail.com>
2016-11-18 09:58:14 +0100

    view_recipe avec jointure swag et tout

--- la_bonne_bouffe/view_recipe.php
+++ la_bonne_bouffe/view_recipe.php
@@ -2,19 +2,19 @@
 
 require_once 'inc/connect.php';
 
-if(isset($_GET['id']) && !empty($_GET['id'])){
+if(isset($_GET['id']) && is_numeric($_GET['id']) && !empty($_GET['id'])){
 
-	$select = $bdd->prepare('SELECT * FROM lbb_recipe LEFT JOIN lbb_users ON id=id_author WHERE id = :idRecipe');
+	$select = $bdd->prepare('SELECT * FROM lbb_recipe LEFT JOIN lbb_users ON lbb_recipe.username_author=lbb_users.username WHERE lbb_recipe.id = :idRecipe'); //requête d'Anthony (pour l'écriture): SELECT * FROM recipe LEFT JOIN users ON recipe.id_autor=users.id;
+
 	$select->bindValue(':idRecipe', $_GET['id'], PDO::PARAM_INT);
 
 	if($select->execute()){
+
 		$recipe = $select->fetch(PDO::FETCH_ASSOC);
 
 	}else{
 		var_dump($select->errorInfo());
 	}
-}else{
-	$error = 'La recette n\'existe pas';
 }
 ?>
 <!DOCTYPE html>
@@ -35,26 +35,30 @@ if(isset($_GET['id']) && !empty($_GET['id'])){
 	<?php include_once 'inc/header.php'; ?>
 
 	<main>
+	<?php if(empty($recipe)):?>
+		<p class="jumbotron text-center alert-danger">La recette n'existe pas </p>
+	<?php else:?>	
 		<section id="section-view-recipe">
 			<h1 class="title-section-list"><?=$recipe['title'];?></h1>
 			<div class="contain-img-list-recipe">
 				<img src="<?=$recipe['picture'];?>" alt="migale" class="img-list-recipe">
 			</div>
-			<div>
+			<!-- <div>
 				<h2 class="title-ingredient-preparation">Ingrédients</h2>
 				<ul>
 					<li>Oeufs</li>
 					<li>Migales</li>
 					<li>Mayo</li>
 				</ul>
-			</div>
+			</div> -->
 			<div>
 				<h2 class="title-ingredient-preparation">Préparation de la recette</h2>
 				<p><?=$recipe['content']?></p>
 			</div>
-			<p class="date-author-recipe">Publié le <?=$recipe['date_publish']?> par <?=$recipe['username']?></p>
+			<p class="date-author-recipe">Publié le <?=$recipe['date_publish']?> par <?='Chef '.$recipe['firstname'].' '.$recipe['lastname'];?></p>
 
-		</section>			
+		</section>
+	<?php endif; ?>				
 	</main>
 
 	<?php include_once 'inc/footer.php'; ?>
